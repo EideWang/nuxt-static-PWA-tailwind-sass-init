4d42ecb9fca9f1fb589b98582fdd055a02088e4f
Eide Wang <eide@icarry.me>
2021-02-25 18:01:40 +0800

    import font Roboto locally

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,13 +11,14 @@ export default {
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [
-      { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
-      {
-        rel: 'stylesheet',
-        href:
-          'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap',
-      },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      //Google Font Setting, Default use Roboto in /static/fonts locally
+      // { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+      // {
+      //   rel: 'stylesheet',
+      //   href:
+      //     'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap',
+      // },
     ],
   },
 
